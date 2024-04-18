10be867e2904c330175ebae8b9c56864cd1f4975
Coimbra <adsjkbd@gmail.com>
2024-04-18 05:43:01 -0300

    Listagem de Attendees de um evento

--- src/main/java/rocketseat/com/passin/dto/attendee/AttendeesListResponseDTO.java
+++ src/main/java/rocketseat/com/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -1,10 +1,7 @@
 package rocketseat.com.passin.dto.attendee;
 
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-public class AttendeesListResponseDTO {
-    List<AttendeeDetails> attendes;
+public record AttendeesListResponseDTO(List<AttendeeDetails> attendes) {
+
 }
--- src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
@@ -1,8 +1,11 @@
 package rocketseat.com.passin.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.checkin.CheckIn;
 
+import java.util.Optional;
+
 public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
